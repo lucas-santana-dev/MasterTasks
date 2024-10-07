8cbbbeea8c8cc25693dcb8d7ca5edf977d1c6f8c
Lucas Santana <lucasalmeida@ciandt.com>
2024-10-06 23:43:30 -0300

    Remover repositório embutido e adicionar alterações

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <GestureHandlerRootView style={{flex:1}}>
       <StatusBar style="auto" />
-    </View>
+      <HomeScreen/>
+      </GestureHandlerRootView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
